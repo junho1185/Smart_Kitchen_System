fa5d705b659359ff2359215493c545fba5e091f8
junho <junho1185@gmail.com>
2023-05-26 20:59:34 +0900

    [0526] update

--- shelves.py
+++ shelves.py
@@ -1,4 +1,5 @@
 from kivy.uix.popup import Popup
+import time
 class shelves:
     def __init__(self, location_list):
         self.location_list = location_list
@@ -7,3 +8,4 @@ class shelves:
         for location in self.location_list:
             # Put some code to rotate the shelf
             print("rotating to . . .", location)
+            # time.sleep(3)
--- voiceRecognition.py
+++ voiceRecognition.py
@@ -9,7 +9,7 @@ class voiceRecognition:
     def speechToText(self):
 
         # 마이크를 사용해 음성 입력 받기
-        with sr.Microphone() as source:
+        with sr.Microphone(device_index=0, channel=0) as source:
             print("말씀하세요...")
             audio = self.r.listen(source)
 
