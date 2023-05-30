e1ed2974c3145ec8aad7f82a55d12fdf3cb715e0
junho <junho1185@gmail.com>
2023-05-30 21:37:01 +0900

    update

--- util/test/soundDeviceCheck.py
+++ util/test/soundDeviceCheck.py
@@ -1,11 +1,3 @@
-import pyaudio
-import sounddevice
-
-p = pyaudio.PyAudio()
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
-
-for i in range(0, numdevices):
-    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i))
-
+import speech_recognition as sr
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
