7c379dcc2090274d32e7b21643a1d7027a61830a
junho <junho1185@gmail.com>
2023-05-30 23:03:21 +0900

    Pyserial

--- shelves.py
+++ shelves.py
@@ -1,5 +1,13 @@
 from kivy.uix.popup import Popup
 import time
+import serial
+
+# 시리얼 통신을 위한 포트 번호와 전송 속도를 설정합니다.
+port = "COM3"  # 포트 번호
+baudrate = 9600  # 전송 속도를 설정합니다.
+
+# 시리얼 통신을 위한 객체를 생성합니다.
+ser = serial.Serial("COM3", baudrate)
 class shelves:
     def __init__(self, location_list):
         self.location_list = location_list
@@ -8,4 +16,5 @@ class shelves:
         for location in self.location_list:
             # Put some code to rotate the shelf
             print("rotating to . . .", location)
+            ser.write(location)
             # time.sleep(3)
