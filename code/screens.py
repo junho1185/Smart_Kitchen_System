ae5a51639a5a33e16e70496108b55a7c8e052dbe
junho <junho1185@gmail.com>
2023-06-05 13:35:48 +0900

    [0605] added exception handler for voice

--- code/screens.py
+++ code/screens.py
@@ -64,7 +64,7 @@ class MainScreenButton(Screen):
 
         popup_content.add_widget(modeSwitch)
 
-        popup = Popup(title='Setting', content=popup_content, size_hint=(0.5, 0.5))
+        popup = Popup(title='Voice Control Mode', content=popup_content, size_hint=(0.5, 0.5))
         callDismiss = partial(self.popupDismiss, mode, modeSwitch)
         popup.bind(on_dismiss=callDismiss)
         popup.open()
@@ -89,12 +89,16 @@ class MainScreenVoice(Screen):
 
         layout = FloatLayout()
 
+        nameLabel = cLabel(text='Smart Kitchen System', pos_hint={'center_x': 0.5, 'center_y': 0.9})
         mic_button = Button(text='mic', pos_hint={'center_x':0.5, 'center_y':0.5}, size_hint=(0.3, 0.3))
+        self.statusLabel = cLabel(text='버튼을 누르고 원하시는 재료/레시피를 말씀하세요.', pos_hint={'center_x':0.5, 'center_y':0.75})
         sButton = Button(text='Settings', pos_hint={'center_x': 0.9, 'center_y': 0.9}, size_hint=(0.1, 0.1))
 
         mic_button.bind(on_press=self.micFunc)
         sButton.bind(on_press=self.settingPopUp)
 
+        layout.add_widget(nameLabel)
+        layout.add_widget(self.statusLabel)
         layout.add_widget(mic_button)
         layout.add_widget(sButton)
 
@@ -120,7 +124,7 @@ class MainScreenVoice(Screen):
         elif type == 'Ingredient':
             self.switchMaterialShow(name)
         else:
-            pass
+            self.statusLabel.text = "다시 말씀해주세요."
 
 
 
@@ -146,7 +150,7 @@ class MainScreenVoice(Screen):
 
         popup_content.add_widget(modeSwitch)
 
-        popup = Popup(title='Setting', content=popup_content, size_hint=(0.5, 0.5))
+        popup = Popup(title='Voice Control Mode', content=popup_content, size_hint=(0.5, 0.5))
         callDismiss = partial(self.popupDismiss, mode, modeSwitch)
         popup.bind(on_dismiss=callDismiss)
         popup.open()
